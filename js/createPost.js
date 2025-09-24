afa44565290db40390612e8005c83cabc095e958
HenrikLeutscher <HenrikHezo@hotmail.com>
2025-09-24 23:06:04 +0200

    Create a post (from button with fields)
    This is not tested (test and re commit)

---
+++ js/createPost.js
@@ -0,0 +1,54 @@
+import { createPost } from "./api/posts.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("createForm");
+    const errorMessage = document.getElementById("error-message");
+    const imgUrlInput = document.getElementById("imageUrl");
+    const imgPreview = document.getElementById("imagePreview");
+    const imgPreviewContainer = document.getElementById("imagePreviewContainer");
+
+    imgUrlInput.addEventListener("input", () => {
+        const url = imgUrlInput.value;
+        if (url) {
+            imgPreview.src = url;
+            imgPreviewContainer.classList.remove("d-none");
+        } else {
+            imgPreview.src = "";
+            imgPreview.alt = "Image preview will be displayed here";
+        }
+    });
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const imgUrl = document.getElementById("imageUrl").value;
+        const body = document.getElementById("body").value;
+
+        if (!title || !body) {
+            errorMessage.textContent = "Please add a title and body text to your post.";
+            return;
+        }
+
+        const postData =  {
+            title,
+            media: imgUrl ? { url: imgUrl, alt: "Post Image"} : undefined,
+            body,
+        };
+
+        try {
+            const newPost = await createPost(postData);
+            errorMessage.classList.remove("text-danger");
+            errorMessage.classList.add("text-success");
+            errorMessage.textContent = `✅ Post created successfully: "${newPost.title}"`;
+
+            form.reset();
+            setTimeout(() => {
+                window.location.href = "feed.html";
+            }, 3000);
+
+        } catch (err) {
+            console.error(err);
+        }
+    });
+});
