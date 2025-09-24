1c2ec5a01596973919dfb5ff7f3a1dd8f87745eb
HenrikLeutscher <HenrikHezo@hotmail.com>
2025-09-24 14:50:48 +0200

    Login done

---
+++ js/index.js
@@ -0,0 +1,37 @@
+import { loginUser } from "./api/auth.js";
+import { saveToken } from "./utils/storage.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#loginForm");
+  const errorMessage = document.getElementById("error-message");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // Form Values
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+      errorMessage.textContent = "Please make sure both fields are filled out.";
+      return;
+    }
+
+    try {
+      const loginResponse = await loginUser(email, password);
+
+      const token = loginResponse.data.accessToken;
+      saveToken(token);
+
+      console.log("Saved token:", token);
+
+      errorMessage.textContent = "Login successful! Redirecting...";
+      setTimeout(() => {
+        window.location.href = "feed.html";
+      }, 2000);
+    } catch (err) {
+      console.log(err);
+      errorMessage.textContent = err.message;
+    }
+  });
+});
