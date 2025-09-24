f23ae9ae6dac8fd3758394f1cd1dbd87daf9e00b
HenrikLeutscher <HenrikHezo@hotmail.com>
2025-09-24 14:52:52 +0200

    Register done

---
+++ js/register.js
@@ -0,0 +1,46 @@
+// js/register.js
+import { registerUser } from "./api/auth.js";
+import { saveToken } from "./utils/storage.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#registerForm");
+  const errorMessage = document.getElementById("error-message");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // Form Values
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    const userData = {
+      name: username,
+      email: email,
+      password: password,
+    };
+
+    // Makes sure Email is as intended
+    if (!email.endsWith("@stud.noroff.no")) {
+        errorMessage.textContent = "Provided email must end with @stud.noroff.no";
+        return;
+    }
+
+    // Double password check, ensuring password is written correctly
+    if (password !== confirmPassword) {
+        errorMessage.textContent = "Passwords do not match, please try again.";
+        return;
+    }
+
+    try {
+      const data = await registerUser(userData);
+      saveToken(data.accessToken);
+      errorMessage.style.color = "green";
+      errorMessage.textContent = "Registration successful! Redirecting...";
+      setTimeout(() => (window.location.href = "feed.html"), 3000);
+    } catch (err) {
+      errorMessage.textContent = err.message;
+    }
+  });
+});
